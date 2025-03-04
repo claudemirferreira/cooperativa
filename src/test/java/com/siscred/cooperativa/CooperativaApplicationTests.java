8b08355f71e1aa2e5fc321fd22f108707a384522
Claudemir <claudemirramosferreira@gmail.com>
2025-03-04 17:23:03 -0300

    - refactory tese unitario

--- src/test/java/com/siscred/cooperativa/CooperativaApplicationTests.java
+++ src/test/java/com/siscred/cooperativa/CooperativaApplicationTests.java
@@ -2,9 +2,11 @@ package com.siscred.cooperativa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestPropertySource(properties = "spring.kafka.listener.auto-startup=false")
 @SpringBootTest
 class CooperativaApplicationTests {
 
